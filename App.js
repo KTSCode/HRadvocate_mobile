dcfc6dae1fd0ed60c38923700f778233e675e904
Kyle San Clemente <kylesanclemente@gmail.com>
2018-02-13 10:34:25 -0800

    renamed businessSelector and updated eslint

--- App.js
+++ App.js
@@ -1,9 +1,9 @@
 import React from 'react';
-import { Provider } from 'react-redux';
-import { StyleSheet, Text, View } from 'react-native';
+import {Provider} from 'react-redux';
+//import {StyleSheet} from 'react-native';
 
 import store from './store';
-import BusinessSelector from './businessSelector/businessSelector';
+import BusinessSelector from './businessSelector/index';
 
 export default class App extends React.Component {
   render() {
@@ -15,11 +15,11 @@ export default class App extends React.Component {
   }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+//const styles = StyleSheet.create({
+  //container: {
+    //flex: 1,
+    //backgroundColor: '#fff',
+    //alignItems: 'center',
+    //justifyContent: 'center',
+  //},
+//});

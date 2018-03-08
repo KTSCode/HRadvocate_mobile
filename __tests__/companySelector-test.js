f499a8062182a5e4e14cb950e1522a2607f08e7d
Kyle San Clemente <kylesanclemente@gmail.com>
2018-03-08 13:47:37 -0800

    cleaned up for eslint

--- __tests__/companySelector-test.js
+++ __tests__/companySelector-test.js
@@ -1,41 +1,36 @@
-//XXX most of this stuff is from https://github.com/reactjs/redux/blob/master/docs/recipes/WritingTests.md
-import React from 'react'
+import React from 'react';
 import {Provider} from 'react-redux';
-//import thunk from 'redux-thunk'
-//import configureMockStore from 'redux-mock-store'
-import renderer from 'react-test-renderer'
-//import expect from 'expect'
+import renderer from 'react-test-renderer';
 import store from '../store';
 
-import * as actions  from '../companySelector/actions'
-import reducer from '../companySelector/reducer'
-import CompanySelector from '../companySelector/index'
-
-//XXX IDK what these are doing
-//const middlewares = [thunk]
-//const mockStore = configureMockStore(middlewares)
+import reducer from '../companySelector/reducer';
+import CompanySelector from '../companySelector/index';
 
 test('Company selector renders correctly', () => {
-  const tree = renderer.create(
+  const tree = renderer
+    .create(
       <Provider store={store}>
         <CompanySelector />
-      </Provider>
-  ).toJSON();
+      </Provider>,
+    )
+    .toJSON();
   expect(tree).toMatchSnapshot();
 });
 
-
-describe('reducer', () => {
+describe('Reducer gets company data using company code', () => {
   it('should return the initial state', () => {
-    expect(reducer(undefined, {})).toEqual({"activeCompany": null, "companies": []})
-  })
+    expect(reducer(undefined, {})).toEqual({
+      activeCompany: null,
+      companies: [],
+      text: '',
+    });
+  });
 
-  it('should handle SET_ACTIVE_COMPANY', () => {
-    expect(reducer({"activeCompany": null, "companies": []}, {
-      type: 'FETCH_COMPANIES',
-      code: 'abc'
-    })).toEqual({"activeCompany": 'abc', "companies": []})
-  })
-
-
-})
+  //it('should handle SET_ACTIVE_COMPANY', () => {
+  //  expect(reducer({"activeCompany": null, "companies": []}, {
+  //    type: 'FETCH_COMPANIES',
+  //    code: 'abc'
+  //  })).toEqual({"activeCompany": 'abc', "companies": []})
+  //})
+  
+});

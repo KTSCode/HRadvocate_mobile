99d552640d7904ac6cdf7c53fa59134efd6e91f4
Kyle San Clemente <kylesanclemente@gmail.com>
2018-04-08 20:06:19 -0700

    switches to login page when company is found

--- main.js
+++ main.js
@@ -1,41 +1,46 @@
 import React from 'react';
+var {View, Text} = require('react-native');
 import CompanyCodePage from './companySelector/index';
 import Login from './login/index';
-import {Provider, connect} from 'react-redux';
+import {connect} from 'react-redux';
 
 class Main extends React.Component {
   constructor(props) {
     super(props);
-    this.companyNotFound = false;
-  }
-
-  companyFound() {
-    this.prop.dispatch({type: 'COMPANY_FOUND'});
-  }
-
-  loggedIn() {
-    this.props.dispatch({type: 'LOGGED_IN'});
+    //this.companyNotFound = false;
   }
 
   render() {
-    if (this.props.found) {
+    if (this.props.loggedIn) {
+      //FIXME find the correct place for this
+      //this.props.dispatch({type: 'LOGGED_IN'});
       return (
-          <Login dispatch={this.props.dispatch} />
+        <View>
+          <Text>main app</Text>
+        </View>
       );
+    } else if (this.props.companyFound) {
+      //FIXME find the correct place for this
+      //this.props.dispatch({type: 'COMPANY_FOUND'});
+      return <Login dispatch={this.props.dispatch} error={'none'} />;
     } else {
       return (
-          <CompanyCodePage dispatch={this.props.dispatch} error={this.props.companyNotFound} />
+        <CompanyCodePage
+          dispatch={this.props.dispatch}
+          error={this.props.companyNotFound}
+        />
       );
     }
   }
 }
 
 const mapStateToProps = state => {
-  let companyNotFound = (!state.companySelector.found && state.companySelector.code != '')
+  let companyNotFound =
+    !state.companySelector.found && state.companySelector.code != '';
   return {
     ...state,
     companyNotFound: companyNotFound,
-    companyFound: state.root.found,
+    companyFound: state.companySelector.found,
     loggedIn: state.root.login,
   };
 };

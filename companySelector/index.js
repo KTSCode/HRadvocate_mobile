13be1aad2d6f1d962432d6f92319735d76074998
Kyle San Clemente <KTSCode@users.noreply.github.com>
2018-04-25 12:47:11 -0700

    Toast Box!!!! and company selector errors (#12)
    * Styled the company code error
    
    * extracted error message to reusable component and added REQUEST_COMPANY_INFO action

--- companySelector/index.js
+++ companySelector/index.js
@@ -1,38 +1,56 @@
 import React from 'react';
-var {StyleSheet, View, Text, Button, Image} = require('react-native');
+var {StyleSheet, View, Button, Image} = require('react-native');
 import CompanyCodeForm from './companyCodeForm';
+import ToastBox from '../toastBox/index';
 import {Card} from 'react-native-material-ui';
 
 const CompanyCodePage = props => {
-  const HRlogo = require('../images/hradvocate.png')
+  const HRlogo = require('../images/hradvocate.png');
+  // TODO make button uniform across iOS and Android
   return (
-    <View style={styles.MyForm}>
-      {props.error && <Text style={styles.Error}>Error: code not found</Text>}
+    <View style={styles.Container}>
       <View style={{alignSelf: 'center', marginTop: 40}}>
         <Image source={HRlogo} style={{height: 120, width: 350}} />
       </View>
-      <Card>
-        <View style={{padding: 30,}}>
-          <CompanyCodeForm
-            onSubmit={values => {
+      <View style={styles.MyForm}>
+        {props.error && (
+          <ToastBox
+            color="#EE3E4B"
+            title="Company Code Not Found"
+            text="Please Try Again"
+            icon="error"
+          />
+        )}
+        {(props.info || props.error) && (
+          <ToastBox
+            color="#1E98C7"
+            title="Contact HR for Company Code"
+            icon="info"
+          />
+        )}
+        <Card>
+          <View style={{padding: 30}}>
+            <CompanyCodeForm
+              onSubmit={values => {
+                props.dispatch({
+                  type: 'SUBMIT_CODE',
+                  code: values.code.toLowerCase(),
+                });
+              }}
+            />
+          </View>
+        </Card>
+        <View style={{width: 80, marginTop: 100}}>
+          <Button
+            title="SKIP"
+            onPress={() => {
               props.dispatch({
                 type: 'SUBMIT_CODE',
-                code: values.code.toLowerCase(),
+                code: 'abc',
               });
             }}
           />
         </View>
-      </Card>
-      <View style={{width: 80, marginTop: 100}}>
-        <Button
-          title="SKIP"
-          onPress={() => {
-            props.dispatch({
-              type: 'SUBMIT_CODE',
-              code: 'abc',
-            });
-          }}
-        />
       </View>
     </View>
   );
@@ -41,21 +59,25 @@ const CompanyCodePage = props => {
 export default CompanyCodePage;
 var styles = StyleSheet.create({
   MyForm: {
+    justifyContent: 'center',
+    flexDirection: 'column',
+    padding: 10,
+    flex: 1,
+  },
+  Container: {
     backgroundColor: '#3F4952',
     justifyContent: 'space-between',
     flexDirection: 'column',
-    padding: 20,
     flex: 1,
   },
-  ButtonStyle: {
-    color: 'red',
-    margin: 10,
-    padding: 20,
-  },
-  Error: {
-    color: 'red',
-  },
-  CardStyle: {
-    backgroundColor: '#abc',
-  },
 });
+
+// XXX  company info button
+//<Button
+//  title="I DON'T HAVE A CODE"
+//  onPress={() => {
+//    props.dispatch({
+//      type: 'REQUEST_COMPANY_INFO',
+//    });
+//  }}
+///>

381132a14723cc2bc7230246dfff71575b60c4cb
Kyle San Clemente <kylesanclemente@gmail.com>
2018-05-02 12:14:33 -0700

    nav stuff

--- navigation/index.js
+++ navigation/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {Text, View} from 'react-native';
 //import {Toolbar} from 'react-native-material-ui';
 import {DrawerNavigator} from 'react-navigation';
+import {Header} from 'react-native-elements';
 
 const Navigation = props => {
   return (
@@ -15,5 +16,38 @@ const Navigation = props => {
     </View>
   );
 };
+
+//const Navigation = props => {
+//  return (
+//    <View
+//      style={{
+//        flex: 1,
+//      }}>
+//      <Header
+//        leftComponent={{icon: 'menu', color: '#fff'}}
+//        centerComponent={{text: 'MY TITLE', style: {color: '#fff'}}}
+//        rightComponent={{icon: 'home', color: '#fff'}}
+//      />
+//      <Router />
+
+//      <View
+//        style={{
+//          flex: 1,
+//          justifyContent: 'center',
+//          alignSelf: 'center',
+//          width: 100,
+//        }}>
+//        <Button
+//          title="Logout"
+//          onPress={() => {
+//            props.dispatch({
+//              type: 'LOGOUT',
+//            });
+//          }}
+//        />
+//      </View>
+//    </View>
+//  );
+//};
 
 export default Navigation;

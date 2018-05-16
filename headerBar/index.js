b278bd986e8af421eedfc827e775b699ab5c6634
Kyle San Clemente <KTSCode@users.noreply.github.com>
2018-05-16 12:48:05 -0700

    Notifications page (#25)
    * swapped calendar
    
    * fixed up the homepage per scotts requests
    
    * make the notifications page
    
    * fixed the styling on the login form
    
    * added company name to headerbar
    
    * checked off notifications

--- headerBar/index.js
+++ headerBar/index.js
@@ -14,7 +14,7 @@ const headerBar = props => {
               props.navigation.navigate('DrawerOpen');
             }}
           />
-          <Text style={styles.companyText}> Company Name </Text>
+          <Text style={styles.companyText}> {props.company} </Text>
         </View>
       }
       rightComponent={
@@ -27,12 +27,7 @@ const headerBar = props => {
           <Icon
             name="notifications"
             color="#FFF"
-            onPress={() => alert('show notifications')}
-          />
-          <Icon
-            name="person"
-            color="#FFF"
-            onPress={() => alert('open profile')}
+            onPress={() => props.navigation.navigate('Notifications')}
           />
         </View>
       }
@@ -47,7 +42,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     alignItems: 'flex-end',
     justifyContent: 'space-between',
-    width: 80,
+    width: 60,
   },
   leftIcons: {
     flexDirection: 'row',

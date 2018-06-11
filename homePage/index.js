9b6821016aa7cbbd43d93b33c3125f410f3bea8a
Kyle San Clemente <KTSCode@users.noreply.github.com>
2018-06-10 20:40:06 -0700

    added pdf links, added a new ABC logo (#36)

--- homePage/index.js
+++ homePage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import {View, StyleSheet, Text, ScrollView, Alert} from 'react-native';
+import {View, StyleSheet, Text, ScrollView} from 'react-native';
 import {Card} from 'react-native-elements';
 import {CalendarList} from 'react-native-calendars';
 
